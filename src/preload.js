216d939ad38a79a52120d34971178cc411f504b2
Cristopher Baca <cristopher.baca@cetys.edu.mx>
2026-09-06 06:45:58 -0700

    DELETED ALL

--- src/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('mynotes', {
-  load: () => ipcRenderer.invoke('data:load'),
-  save: (data) => ipcRenderer.invoke('data:save', data),
-  exportPdf: (notebook) => ipcRenderer.invoke('notebook:export-pdf', notebook)
-});
